4f763bf1b458237af630106ec951cf441f1dbc89
Max-labb <maxime.cornez@icloud.com>
2025-03-09 11:47:55 +0100

    add penguins

---
+++ penguins.R
@@ -0,0 +1,10 @@
+library(tidyverse)
+library(palmerpenguins)
+
+penguins %>% 
+  ggplot(aes(x = species, y = bill_length_mm)) +
+  geom_boxplot() +
+  theme_minimal() +
+  labs(title = "Boxplot of bill length by species",
+       x = "Species",
+       y = "Bill length (mm)")
